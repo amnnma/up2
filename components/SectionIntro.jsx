5c79310b885c16207861bb26b1a42c138cf057b7
Saknarong Sombutjaroan <umsuperza14@gmail.com>
2021-08-06 15:39:21 +0700

    Fix intro

--- components/SectionIntro.jsx
+++ components/SectionIntro.jsx
@@ -1,13 +1,36 @@
-const SectionIntro = () => {
-    return (
-      <div style={{ background: "#FFD43B"}}>
-        <div className="container flex flex-col justify-around min-h-screen py-10 mx-auto text-center md:py-16">
-          <div className="w-11/12 mx-auto text-white-1 B1 md:w-9/12 lg:w-6/12 ">
-        </div>    
-          </div>
+import arrow_down from "../assets/images/arrow.svg";
+import Intro from "../assets/images/bg.png";
+import Introimage from "../assets/images/Introimage.svg";
+
+
+const SectionIntro = () => { 
+  return (
+    <div className="relative" id="wrapper">
+      <div
+        className="relative min-h-screen"
+        style={{ background: "#FFD43B" }}
+      >
+         <img src={Intro} layout="fill"/>
+         <div className="test" style={{ top: "4%", left: "50%", transform: " translateX(-50%)" }}>
+         <img src={Introimage} width={1200} height={1200}/>
+         </div>
+        <div
+          className="absolute flex flex-col items-center w-11/12 mx-auto text-center md:w-full"
+          style={{ top: "40%", left: "50%", transform: " translateX(-50%)" }}
+        >
+          <p className="flex flex-col font-semibold text-5xl text-white ">
+            <span>Waste is money</span> <span> UPCYCLING is culture</span>
+          </p>
+          <p className="flex-row font-semibold text-white">
+            <span>Upcycling วิถีใหม่แห่งการลดขยะ เพิ่มมูลค่าและช่วยโลก </span>
+          </p>
+        </div>
+        <div className="absolute flex-col text-center md:w-full" style={{ top: "90%"}}>
+        <img src={arrow_down}/>
+        </div>
       </div>
-    );
-  };
-  
-  export default SectionIntro;
-  
+    </div>
+  );
+};
+
+export default SectionIntro;
