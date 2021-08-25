6b8ebc6ef1e4238c2633ff7967f4554a4eb7b40c
amnnma <blackpotter14@gmail.com>
2021-08-25 22:09:58 +0700

    fix image ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,8 @@
-declare module '*.png'
-declare module '*.svg'
+declare module '*.png' {
+    const value: any;
+   export = value;
+}
+declare module '*.svg' {
+    const value: any;
+   export = value;
+}
