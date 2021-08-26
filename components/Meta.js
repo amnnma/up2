f8a410cc7b30188cb5b883d5d836de25f3b046af
amnnma <blackpotter14@gmail.com>
2021-08-26 08:25:19 +0700

    meta error

--- components/Meta.js
+++ components/Meta.js
@@ -7,13 +7,13 @@ const Meta = () => {
   const web_url = config.web_url
   const title = 'Waste is money Upcycling is culture'
   const description = 'Upcycling วิถีใหม่แห่งการลดขยะ เพิ่มมูลค่าและช่วยโลก'
-  const og_image = '/dist/upcycling/og_image.png'
+  const og_image = '/upcycling/og_image.png'
 
   return (
     <>
       <Head>
       <title lang='th' >{title}</title>
-        <link rel="icon" type="image/x-icon" href="/dist/upcycling/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/upcycling/favicon.ico" />
         <meta name="description" content={description}></meta>
 
         <meta property="og:title" content={title} />
