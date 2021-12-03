618e5a9d20098a7eac5a2e0acfca9739bfc9b2ba
BP <umsuperza14@gmail.com>
2021-12-03 12:34:39 +0700

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import SectionMap from "../components/SectionMap";
 import SectionIntro from "../components/SectionIntro";
 import SectionIntroblue from "../components/SectionIntroblue";
-import SectionIntroblue2 from "../components/SectionIntroblue2";
-import SectionIntroblue3 from "../components/SectionIntroblue3";
+
 import SectionWaste from "../components/SectionWaste";
 import SectionWaste2 from "../components/SectionWaste2";
 import SectionWaste3 from "../components/SectionWaste3";
@@ -13,13 +12,13 @@ import SectionWaste7 from "../components/SectionWaste7";
 import SectionWaste8 from "../components/SectionWaste8";
 import SectionWaste9 from "../components/SectionWaste9";
 import SectionUpcycleIntro from "../components/SectionUpcycleIntro";
-import SectionQuiz from "../components/SectionQuiz";
+
 import SectionMaterial from "../components/SectionMaterial";
-import SectionMaterial2 from "../components/SectionMaterial2";
+
 import SectionCardflip from "../components/SectionCardflip";
 import SectionCardflip2 from "../components/SectionCardflip2";
 import SectionFooter from "../components/SectionFooter";
-import SectionFooter2 from "../components/SectionFooter2";
+
 import Header from "./Header";
 import Sixth from "../components/Sixth";
 export default function Home() {
