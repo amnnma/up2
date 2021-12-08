25e5427647c9d80b91cf7d1ee2226c320a0d8175
BP <umsuperza14@gmail.com>
2021-12-08 13:12:08 +0700

    disable locomotive

--- pages/_app.js
+++ pages/_app.js
@@ -13,26 +13,11 @@ function MyApp({ Component, pageProps }) {
     <>
       <Meta />
 
-      <LocomotiveScrollProvider
-        options={{
-          smooth: true,
-          // ... all available Locomotive Scroll instance options
-        }}
-        watch={
-          [
-            //..all the dependencies you want to watch to update the scroll.
-            //  Basicaly, you would want to watch page/location changes
-            //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-          ]
-        }
-        containerRef={containerRef}
-      >
-        <main data-scroll-container ref={containerRef}>
-          <SafeHydrate>
+        <SafeHydrate>
             <Component {...pageProps} />
-          </SafeHydrate>
-        </main>
-      </LocomotiveScrollProvider>
+        </SafeHydrate>
+    
+   
     </>
   );
 }
